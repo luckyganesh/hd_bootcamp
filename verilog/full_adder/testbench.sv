5ef647df080d0dbf36b39049b3df490a46adbe00
Sai Ganesh <44020019+luckyganesh@users.noreply.github.com>
2019-12-11 12:27:05 +0530

    Add display in full adder test bench

--- verilog/full_adder/testbench.sv
+++ verilog/full_adder/testbench.sv
@@ -11,20 +11,28 @@ module fulladder_testbench;
 	    $dumpfile("dump.vcd");
     	$dumpvars(1);
           a = 0;b = 0;c = 0;
+          display;
           #10
           a = 0;b = 0;c = 1;
+          display;
           #10
           a = 0;b = 1;c = 0;
+          display;
           #10
           a = 0;b = 1;c = 1;
+          display;
           #10
           a = 1;b = 0;c = 0;
+          display;
           #10
           a = 1;b = 0;c = 1;
+          display;
           #10
           a = 1;b = 1;c = 0;
+          display;
           #10
           a = 1;b = 1;c = 1;
+          display;
         end
   
   task display;
@@ -33,4 +41,3 @@ module fulladder_testbench;
 
   
 endmodule
-
